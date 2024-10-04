5cce259a2f3eba05e57a7490cc109259c57d2047
Rukhsar Yousufi <byousufi1@nait.ca>
2024-10-04 16:40:38 -0600

    Assignemt-1

--- frontend/js/api/jobs.js
+++ frontend/js/api/jobs.js
@@ -1,1 +1,70 @@
-// your code goes here.
+
+import {  displayJobs } from '../index.js';
+
+async function searchJobs(searchQuery) {
+    try {
+        const response = await fetch(`http://localhost:3000/jobs?q=${encodeURIComponent(searchQuery)}`);
+
+        if (!response.ok) {
+            throw new Error(`An error occurred: ${response.statusText}`);
+        }
+
+        const jobs = await response.json();
+
+        displayJobs(jobs);
+    } catch (error) {
+        console.error("Failed to fetch jobs:", error);
+    }
+}
+
+async function viewJobDetails(jobId) {
+    try {
+        const response = await fetch(`http://localhost:3000/jobs/${jobId}`); 
+
+        if (!response.ok) {
+            throw new Error(`An error occurred: ${response.statusText}`);
+        }
+
+        const jobDetails = await response.json();
+        displayJobDetails(jobDetails); 
+    } catch (error) {
+        console.error("Failed to fetch job details:", error);
+    }
+}
+
+function displayJobDetails(job) {
+    const jobDetailsCard = document.getElementById('job-details-card');
+    jobDetailsCard.innerHTML = `
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title">${job.title}</h3>
+                <h4 class="card-subtitle mb-2 text-body-secondary pb-3">${job.company}</h4>
+                <h6 class="card-subtitle mb-2 text-body-secondary">${job.location}</h6>
+                <h6 class="card-subtitle mb-2 text-body-secondary pb-3">Posted ${formatJobDate(job.date_posted)}</h6>
+                <h5 class="card-subtitle mb-2">Description</h5>
+                <p class="card-text">${job.description}</p>
+                <h5 class="card-subtitle mb-2">Qualifications</h5>
+                <p class="card-text">${job.qualifications}</p>
+                <button class="btn btn-success save-job">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16">
+                        <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
+                    </svg>
+                    Save Job
+                </button>
+            </div>
+        </div>
+    `;
+
+}
+
+
+function formatJobDate(postedDate) {
+    const date = new Date(postedDate);
+    return date.toLocaleDateString('en-US', {
+        month: 'numeric', 
+        day: 'numeric',
+        year: 'numeric'
+    });
+}
+
+export { searchJobs, viewJobDetails };
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,78 @@
-// your code goes here.
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { searchJobs, viewJobDetails } from './api/jobs.js'; 
+
+
+document.getElementById('search-jobs-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const searchQuery = document.getElementById('query-input').value;
+    console.log(searchQuery)
+
+    searchJobs(searchQuery);
+});
+
+
+function displayJobs(jobs) {
+    const jobsContainer = document.getElementById('searched-jobs');
+    jobsContainer.innerHTML = '';  
+    if (jobs.length === 0) {
+        jobsContainer.innerHTML = `<div class="text-dark">No Results Found</div>`;
+        return;
+    }
+
+    jobs.forEach(job => {
+        const jobElement = document.createElement('li');
+        jobElement.classList.add('job-card', 'card', 'my-1');
+        jobElement.style.width = '18rem';
+
+        jobElement.innerHTML = `
+            <div class="card-header">${job.company}</div>
+            <div class="card-body">
+                <h5 class="card-title">${job.title}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">${job.location}</h6>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Posted ${formatJobDate(job.date_posted)}</h6>
+                <button class="btn btn-primary view-job-button" job-data-id="${job.id}">View Job</button>
+            </div>
+        `;
+
+        jobsContainer.appendChild(jobElement);
+
+        jobElement.querySelector('.view-job-button').addEventListener('click', function() {
+            const jobId = job.id;
+            viewJobDetails(jobId);
+        });
+        
+
+    });
+}
+
+function formatJobDate(postedDate) {
+    const date = new Date(postedDate);
+    return date.toLocaleDateString('en-US', {
+        month: 'numeric', 
+        day: 'numeric',
+        year: 'numeric'
+    });
+}
+
+
+window.onload = function() {
+    fetchAllJobs();
+};
+
+async function fetchAllJobs() {
+    try {
+        const response = await fetch('http://localhost:3000/jobs'); 
+        if (!response.ok) {
+            throw new Error(`An error occurred: ${response.statusText}`);
+        }
+
+        const jobs = await response.json();
+        displayJobs(jobs);
+    } catch (error) {
+        console.error("Failed to fetch jobs:", error);
+    }
+}
+
+
+export { displayJobs };
